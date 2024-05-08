dd8973052c13873844493db8725ef850fee1e885
Shaun Livingston <s.livingston@traxion.global>
2024-05-08 14:25:35 -0600

    grid dp

--- grid_paths.py
+++ grid_paths.py
@@ -1,4 +1,25 @@
-# Buttom Up Dynamic Programming approach to number of paths through a grid
+"""
+Problem:
+find the number of paths you can take through a grid
+you can only go right and down (which also means you can only enter along the left and top sides)
+
+
+Solution:
+Buttom Up Dynamic Programming
+Basic idea is that the number of ways to get to a grid cell is the sum of:
+  1. all the ways to get to the cell on the right
+  2. plus all the ways to get to the cell above you
+(but the entry points/boundaries must be initialized as 1)
+
+time complexity: avg/worst case are all the same - O(n^2) (must visit all the rows and columms once)
+space complexity: avg/worst case are all the same - O(n^2) (create a dp table of n*n size)
+
+this same approach can be extended to problems like:
+- adding values to each of the cells to create sort of a topography
+- find the fastest way through a grid (use min values from predeeding cels + 1 at each cell)
+- coin change problem 1 - smallest number of coins to get to a value
+- coin change problem 2 - number of ways to use coins to get to a value
+"""
 
 def num_paths(n):
 	# find the total number of possible paths in an n x n grid
@@ -20,3 +41,5 @@ def num_paths(n):
 			print(col, end = " ")
 		print()
 	return grid[-1][-1]
+
+print(num_paths(10))
