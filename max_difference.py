2445a9c2a254e23bf7eb173c566fab07c5f9a7a6
Shaun Livingston <s.livingston@traxion.global>
2024-05-08 21:28:12 -0600

    max diff

--- max_difference.py
+++ max_difference.py
@@ -1,18 +1,21 @@
-# Python 3 code to find Maximum difference 
-# between two elements such that larger 
-# element appears after the smaller number 
+"""
+Problem:
+Find Maximum difference between two elements such that larger element appears after the smaller number
+Assume that there are at least two elements in array. 
+Return a negative value if the array is sorted in decreasing order.
+Return 0 if elements are equal
 
-# The function assumes that there are 
-# at least two elements in array. 
-# The function returns a negative 
-# value if the array is sorted in 
-# decreasing order. Returns 0 if 
-# elements are equal 
-def maxDiff(arr, arr_size): 
-    max_diff = arr[1] - arr[0] 
-    min_element = arr[0] 
+Solution:
+Set the min element as the first in the list
+for each (starting from second):
+ - calculate the diff between the lowest and current element, set max diff to lowest(current diff, previous highest diff)
+ - reset lowest if current is lower than the previous lowest
+"""
+def forward_max_diff(arr: list[int]) -> int: 
+    max_diff = arr[1] - arr[0]  # initialize to the diff between the first two elements
+    min_element = arr[0] # first element is by default the lowest (and highest)
     
-    for i in range( 1, arr_size ): 
+    for i in range(1, len(arr)): 
         if (arr[i] - min_element > max_diff): 
             max_diff = arr[i] - min_element 
     
@@ -21,7 +24,5 @@ def maxDiff(arr, arr_size):
     return max_diff 
     
 # Driver program to test above function 
-arr = [1, 2, 6, 80, 100] 
-size = len(arr) 
-print ("Maximum difference is", 
-        maxDiff(arr, size)) 
+arr = [10, -2, 6, 80, 100]
+print("Maximum difference is", forward_max_diff(arr)) 
